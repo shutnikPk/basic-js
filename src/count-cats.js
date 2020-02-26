6843c5729e8a2f7d13e7c2a6777a67b55f520b1d
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2020-02-26 15:58:15 +0300

    feat: add solotion in countCats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  let counter=0;
+  if(matrix==undefined||matrix.length==0){
+    return counter;
+  }else
+  {
+    for(let i = 0; i < matrix.length; i++){
+      while (matrix[i].indexOf('^^')!=-1){
+        matrix[i][matrix[i].indexOf('^^')]=null;
+        counter++;
+      }
+    }return counter;
+  }
+
 };
